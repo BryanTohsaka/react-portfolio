08bd225769acfbbda9458011467f387eb684d8dd
Tohsaka <bryan_avina@hotmail.com>
2022-03-23 18:13:21 -0600

    resseteo de estilos y carga de componentes a app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import Nav from './components/Nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/porfolio/Portfolio'
+import Testimonials from './components/testimonials/Testimonials'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 const app = () => {
     return (
         <>
         <Header />
-        <div>hola</div>
         <Nav />
+        <About />
+        <Experience />
+        <Services />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
         </>
     )
 }
