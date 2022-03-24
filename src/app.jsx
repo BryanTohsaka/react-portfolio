1ea1bcdf8a61e1d2960796d9387824480cf654a7
Bryan Tohsaka <shirakotosaka@gmail.com>
2022-03-23 20:55:25 -0600

    reparacion de archivos traidos desde git hub

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Header from './components/Header/Header'
-import Nav from './components/Nav/Nav'
-import About from './components/about/About'
-import Experience from './components/experience/Experience'
-import Services from './components/services/Services'
-import Portfolio from './components/porfolio/Portfolio'
-import Testimonials from './components/testimonials/Testimonials'
-import Contact from './components/contact/Contact'
-import Footer from './components/footer/Footer'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Services from './components/services/services'
+import Portfolio from './components/porfolio/portfolio'
+import Testimonials from './components/testimonials/testimonials'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
 const app = () => {
     return (
         <>
