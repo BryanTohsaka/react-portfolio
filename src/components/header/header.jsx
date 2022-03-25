e45fdea077be1bfe6d5efac420f93f6d52d16c77
Tohsaka <bryan_avina@hotmail.com>
2022-03-25 12:46:15 -0600

    creacion de componentes para el header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello i'm</h5>
+        <h1>Tohsaka</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
